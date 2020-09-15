cab7f73e8726ed47ca16b4ce96f87d1fd380a87a
Spidy-crypto <63151411+Spidy-crypto@users.noreply.github.com>
2020-09-15 21:27:01 +0530

    Add files via upload

--- js/apply.js
+++ js/apply.js
@@ -1,27 +1,39 @@
+document.getElementById("loginform").onsubmit = login;
+function login(e) {
+    e.preventDefault();
+    let uname = document.getElementById("username");
+    let pass = document.getElementById("password");	
+    if (uname.value != "admin" || pass.value != "admin") {
+        swal("Try again!", "Invalid Username or Password", "warning");
+    } else {
+        document.getElementById("loginform").submit();
+    }
+}
+
 document.getElementById("regform").onsubmit = show;
 
 function show(e) {
+
+    let otp = Math.floor(Math.random() * 1000) + 1000;
+    alert("Enter this as otp :- " + otp);
     e.preventDefault();
-    var pass = document.getElementById('password');
-    var repass = document.getElementById('repassword');
+    let pass = document.getElementById('password');
+    let repass = document.getElementById('repassword');
     if (pass.value != repass.value) {
         pass.value = "";
         repass.value = "";
         swal("Try again!", "Password and Confirm-password should be same", "error");
     } else {
-        swal("Good Job!", "You are successfully reegistered", "success").then(() => { document.getElementById("regform").submit() });
-    }
-}
-document.getElementById("loginform").onsubmit = login;
-
-function login(e) {
-    e.preventDefault();
-    var uname = document.getElementById("username");
-    var pass = document.getElementById("password");
-    if (uname.value != "admin" || pass.value != "admin") {
-        swal("Try again!", "Invalid Username or Password", "warning");
-    } else {
-        document.getElementById("loginform").submit();
+        swal("Enter Your Otp ", {
+            content: "input",
+            placeholder: "Enter Otp"
+        }).then((value) => {
+            if (value == otp) {
+                swal("Good Job", "You have successfully registered", "success").then(() => { document.getElementById("regform").submit() });
+            } else {
+                swal("", "Enter Correct Otp", "error");
+            }
+        });
     }
 }
 
@@ -36,5 +48,23 @@ function summer() {
         focus: true,
         placeholder: 'write here...'
     });
-    
+
+}
+
+function admin(){
+	$(document).ready(function() {
+
+		swal("To remove any User click on that row");
+         
+        $("#search").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $(".table tr2").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+
+        $("tbody tr").click(function(){
+        	$(this).remove();
+        });
+    });
 }
